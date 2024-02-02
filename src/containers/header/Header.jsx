baa6c969bdcbfe6d6b926b498bc48e20f118d724
Carlos Eduardo <caduadosiqueira@gmail.com>
2024-02-02 16:35:28 -0300

    cv for download

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,6 +3,7 @@ import "./header.css";
 import Loop from "../../components/reactLoop/Loop";
 
 import {motion} from "framer-motion";
+import { FaFileDownload } from "react-icons/fa";
 
 const Title = ({text, duration}) =>(
     <>
@@ -25,13 +26,18 @@ const Par = ({text, duration}) =>(
 
 
 function Header(){
+        const publicDir = process.env.PUBLIC_URL
 
         return(<div className="portifolio__header-background section_padding">
         <section className="portifolio__header_section">
+        
             <div className="portifolio__header_section-title">
                 <Title text={"Hello, i'm Carlos Eduardo"} duration = {1}/>
                 <Title text={"Wellcome to my personal portifoilio"} duration = {2}/>
                 <Par text={"I developed this web page to challenge myself and show my evolution in react.js, I show you a little about the projects I've done, a little about myself and my contacts"} duration = {3}/>
+                <div className="curriculum-download_div">
+                    <a href={`${publicDir}/Carlos_Siqueira_CV.pdf`} download={"Carlos_Siqueira_CV.pdf"} className="curriculum-download gradient__text" id="download_cv"><FaFileDownload size={25} color="gray" /> Download my curriculum</a>
+                </div>
             </div>
             <div className="portifolio__header_section-image">
                 <div className="portifolio__header_section-image_info1 slit-in-vertical">
